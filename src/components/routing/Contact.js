878c45c1440e552b7af4cb4c0189d6d7ea22cffb
awalone <migoyeng@gmail.com>
2020-03-09 10:22:41 +0800

    penambahan routing

---
+++ src/components/routing/Contact.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Contact</h4>
+                <p>This is Contact page</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/routing/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class NotFound extends Component {
+    render() {
+        return (
+            <div>
+                <h2 style={{marginBottom: 0}}>404</h2>
+                <h4 style={{marginTop: 0}}>Page Not Found !</h4>
+            </div>
+        )
+    }
+}
