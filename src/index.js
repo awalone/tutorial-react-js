a6b1acd89832fdde64005d8268225c014c6f3b5a
awalone <migoyeng@gmail.com>
2020-03-11 11:00:11 +0800

    belajar api dengan menggunakan fetch

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import './index.css';
 // import App from './App';
 import Routing from './components/routing/Routing'
 import SubRouting from './components/routing-nested/SubRouting'
-
+import Api from './components/api-call/api'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<SubRouting />, document.getElementById('root'));
+ReactDOM.render(<Api />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
