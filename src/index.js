5678d8f99bb0fadc177bad8baea1efbc3312e229
awalone <migoyeng@gmail.com>
2020-03-11 12:49:48 +0800

    api hook

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import './index.css';
 import Routing from './components/routing/Routing'
 import SubRouting from './components/routing-nested/SubRouting'
 import Api from './components/api-call/api'
+import ApiHook from './components/api-call-hook/api_hook'
 import Binding from './components/handle-event/binding'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Binding />, document.getElementById('root'));
+ReactDOM.render(<ApiHook />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
